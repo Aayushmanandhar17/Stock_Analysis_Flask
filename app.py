622708101c59db51c90c427ab2b897ca1fd2e56d
Aayush Manandhar <34245763+Adammanandhar@users.noreply.github.com>
2020-10-25 15:35:29 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -21,49 +21,39 @@ test=visual_class()
 def search():
     company=request.form.get("symbol")
     test.company_name=company
-
-    ##test.value=y
     return flask.redirect("/")
-
-@app.route('/test')
-def testing():
-    stock_result=stock_data(test.company_name)
-
-    return render_template('test.html', data=test.company_name, value=stock_result)
-
-
-def stock_data(name):
-    start=datetime.datetime(2020,8,29)
-    end=datetime.datetime(2020,9,29)
-
-    STOCK=web.DataReader(test.company_name,'yahoo',start,end)
-    result = STOCK['Close']
-    return result
-
-
-def convert_json(data):
-    new_result=data.to_json(orient="values")
-    parsed = json.loads(new_result)
-    x=json.dumps(parsed, indent=4)
-    y=json.loads(x)
-    return y
 
 
 @app.route('/')
 def index():
-    stock_result=stock_data(test.company_name)
-    json_data=convert_json(stock_result)
+    stock_result=test.stock_data(test.company_name)
+    json_data=test.convert_json(stock_result)
+    Name, company_description,sector,dividend,eps,capital,exchange,peratio=test.company_description(test.company_name)
     data.close_price=json_data
-    return render_template('chart.html',data=test.company_name, value=json_data)
+    return render_template('chart.html',data=Name,Description=company_description,sector=sector,dividend=dividend,eps=eps,exchange=exchange,peratio=peratio,capital=capital )
 
 ## Creatign a json object of the Data
 @app.route('/data')
 def data():
-    stock_result=stock_data(test.company_name)
-    json_data=convert_json(stock_result)
+    stock_result=test.stock_data(test.company_name)
+    json_data=test.convert_json(stock_result)
     return jsonify({'results':json_data })
+
+@app.route('/data_strategy')
+def data_strategy():
+
+    json_data=test.convert_json(stock)
+    return jsonify({'results':json_data})
+
+@app.route('/pipe', methods=["GET", "POST"])
+def pipe():
+    stock_result=test.dual_moving_average(test.company_name)
+
+    stock_result['Buy_Signal'],stock_result['Sell_Signal']=test.buy_sell(stock_result)
+    json_data=test.convert_json(stock_result)
+    return jsonify({'res':json_data})
 
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,threaded=True)
--- visual.py
+++ visual.py
@@ -1,11 +1,91 @@
 import flask
 from flask import render_template, jsonify,json
 import pandas as pd
+import numpy as np
 import datetime
 import pandas_datareader.data as web
+import requests
+from bs4 import BeautifulSoup
 class visual_class:
     company_name="TSLA"
     close_price=0
+    stock=0
     def __init__(self):
-
+        self.alpha_vintage="4X4XP17E6TN4ULP5"
+        self.stock=self.dual_moving_average(self.company_name)
+        self.buy,self.sell=self.buy_sell(self.stock)
         print("Constructor created")
+
+
+    def stock_data(self,name):
+        start=datetime.datetime(2020,8,29)
+        end=datetime.datetime(2020,9,29)
+
+        STOCK=web.DataReader(self.company_name,'yahoo',start,end)
+        result = STOCK['Close']
+        return result
+
+
+    def convert_json(self,data):
+        new_result=data.to_json(orient="values")
+        parsed = json.loads(new_result)
+        x=json.dumps(parsed, indent=4)
+        y=json.loads(x)
+        return y
+    def company_description(self,name):
+        data=requests.get(f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={name}&apikey={self.alpha_vintage}")
+        data=data.json()
+        description=data['Description']
+        sector=data['Sector']
+        dividend=data['DividendPerShare']
+        EPS=data['EPS']
+        capital=data['MarketCapitalization']
+        Name=data['Name']
+        exchange=data['Exchange']
+        peratio=data['PERatio']
+
+
+        return Name, description,sector,dividend,EPS,capital,exchange,peratio
+
+    def dual_moving_average(self,name):
+        start=datetime.datetime(2015,10,18)
+        end=datetime.datetime(2020,10,18)
+        STOCK=web.DataReader(name,'yahoo',start,end)
+
+        STOCK['MA30']=STOCK['Close'].rolling(30).mean()
+        STOCK['MA100']=STOCK['Close'].rolling(100).mean()
+        STOCK.reset_index(inplace=True)
+
+        return STOCK
+
+
+#Creating a function that lets the user when to buy and sell
+    def buy_sell(self,data):
+
+        sigBuy=[]
+        sigSell=[]
+        flag=-1
+
+        for i in range(len(data)):
+            if data['MA30'][i]>data['MA100'][i]:
+                if flag!=1:
+                    sigBuy.append(data['Close'][i])
+                    sigSell.append(np.nan)
+                    flag=1
+                else:
+                    sigBuy.append(np.nan)
+                    sigSell.append(np.nan)
+
+            elif data['MA30'][i]<data['MA100'][i]:
+                if flag!=-1:
+                    sigBuy.append(np.nan)
+                    sigSell.append(data['Close'][i])
+                    flag=-1
+                else:
+                    sigBuy.append(np.nan)
+                    sigSell.append(np.nan)
+            else:
+                sigBuy.append(np.nan)
+                sigSell.append(np.nan)
+
+        return(sigBuy,sigSell)
