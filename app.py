927d10ca16a6c702cf56feaf3bbad660d4d47be6
Aayush <aayush.manandhar17@gmail.com>
2020-11-28 08:49:49 -0500

    Adding completly new features

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import flask
 import os
+import io
+import base64
 from flask import Flask, render_template, jsonify,json
 from jinja2 import Template
 from visual import *
@@ -8,6 +10,14 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import keras
+from keras import backend as k
+from keras import Sequential
+from keras.models import load_model
+from keras.models import model_from_json
+
+from sklearn.preprocessing import MinMaxScaler
 
 import pandas_datareader.data as web
 import pandas_datareader
@@ -29,8 +39,12 @@ def index():
     stock_result=test.stock_data(test.company_name)
     json_data=test.convert_json(stock_result)
     Name, company_description,sector,dividend,eps,capital,exchange,peratio=test.company_description(test.company_name)
+    test.load_trained_model('BAC_pred.json','BAC_predictor.h5')
+
+    print("The prediction for next day is: ",test.single_day)
+    news_data,polarity,subjectivity=test.news()
     data.close_price=json_data
-    return render_template('chart.html',data=Name,Description=company_description,sector=sector,dividend=dividend,eps=eps,exchange=exchange,peratio=peratio,capital=capital )
+    return render_template('chart.html',data=Name,Description=company_description,sector=sector,dividend=dividend,eps=eps,exchange=exchange,peratio=peratio,capital=capital, news=news_data,polar=polarity,subject=subjectivity )
 
 ## Creatign a json object of the Data
 @app.route('/data')
@@ -48,10 +62,11 @@ def data_strategy():
 @app.route('/pipe', methods=["GET", "POST"])
 def pipe():
     stock_result=test.dual_moving_average(test.company_name)
-
     stock_result['Buy_Signal'],stock_result['Sell_Signal']=test.buy_sell(stock_result)
     json_data=test.convert_json(stock_result)
-    return jsonify({'res':json_data})
+    prediction_data=test.convert_json(test.pred)
+
+    return jsonify({'res':json_data,'pred':prediction_data})
 
 
 
