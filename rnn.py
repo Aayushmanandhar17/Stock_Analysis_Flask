86169bb72b517b6acf038b1da160664e91da2bf5
Aayush <aayush.manandhar17@gmail.com>
2021-01-24 22:51:39 -0500

    no file found exception fixed

--- rnn.py
+++ rnn.py
@@ -1,5 +1,6 @@
 
 import visual as vc
+import os
 import numpy as np
 import json
 import tensorflow as tf
@@ -19,18 +20,26 @@ class machine_learning:
         self.start_price_sixty=0
         self.sc=MinMaxScaler(feature_range=(0,1))
         self.company_name="TSLA"
+        self.set_deafult_h5_path="trained_models\TSLA.h5"
+        self.set_deafult_json_file="trained_models\TSLA.json"
 
-# Loading the trained model
 #TO DO
-# work on file not found exception
 # Train multiple company stock Price
-
-
-
     def get_h5_json(self):
         json_file=f"trained_models\{self.company_name}.json"
         hfive_file=f"trained_models\{self.company_name}.h5"
-        return json_file, hfive_file
+        if self.file_exists(hfive_file):
+            return json_file, hfive_file
+        else:
+            print("The file does not exists!!, using Tesla Trained model")
+            json_file=self.set_deafult_json_file
+            hfive_file=self.set_deafult_h5_path
+            return json_file,hfive_file
+
+    def file_exists(self,hfive):
+        file_exists=os.path.exists(hfive)
+        if file_exists:
+            return True
 
     def load_trained_model(self):
         json_file,hfive_file=self.get_h5_json()
@@ -46,10 +55,8 @@ class machine_learning:
     def set_rnn_datastructure(self, name):
         data_frame=data.dual_moving_average(name)
         df=data_frame[['Open','Volume']]
-
         sixty_day_df=data_frame[-60:]
         sixty_day_df.reset_index(inplace=True)
-
         Train_60_days=df.tail(120)[:60]
         Test_60_days=df.tail(60)
         sixty_day_price= np.array(df['Open'].tail(60))
@@ -85,18 +92,13 @@ class machine_learning:
         graph=tf.get_default_graph()
         data_frame=data.dual_moving_average(name)
         last_30_days=data_frame[['Open','Volume']][-60:]
-
         inputs_single=self.sc.fit_transform(last_30_days)
-
         Single_data=[]
         for i in range(60,61):
             Single_data.append(inputs_single[i-60:i])
         Single_data=np.array(Single_data)
-
         Single_final_prediction=self.model.predict(Single_data)
         s=self.sc.scale_
         scaler=(1/self.scale[0])
-
         Single_final_prediction=(Single_final_prediction*scaler)+self.bias
-
         self.single_day= Single_final_prediction
--- test_rnn.py
+++ test_rnn.py
@@ -3,5 +3,6 @@ import rnn
 test=rnn.machine_learning()
 
 h5,jso=test.get_h5_json()
+
 print(h5)
-print(type(jso))
+print(jso)
