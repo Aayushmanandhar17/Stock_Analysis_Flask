434f37c93c648b5fea777c0734e5df273fd370d2
Adammanandhar <aayush.manandhar17@gmail.com>
2021-02-01 22:11:10 -0500

    created a function for metrics

--- news_analysis.py
+++ news_analysis.py
@@ -11,7 +11,7 @@ class news_data:
         self.finnhub_api='c0c76rn48v6u6kubfclg'
         self.news_json=None
 
-    def news(self,symbol='TSLA'):
+    def news_json_api(self,symbol='TSLA'):
         start,end=yahoo_data.get_start_end_date(2)
         r = requests.get(f'https://finnhub.io/api/v1/company-news?symbol={symbol}&from={start}&to={end}&token={self.finnhub_api}')
         data=r.json()
@@ -25,33 +25,35 @@ class news_data:
             result.append(head)
         return result
 
-    def metrics(self,symbol,attribute):
-        pass
 
+    def metrics(self,symbol='TSLA',attribute='headline'):
+        news=self.news_json_api(symbol)
+        headline=self.news_attribute(attribute)
+        merged_data=filter.clean_data(headline)
+        sen=filter.getSubjectivity()
+        pol=filter.getPolarity()
+        SIA=filter.getSIA()
+        stock_price=yahoo_data.today_yahoo_data()
+        data={'Open':stock_price['Open'],
+              'High':stock_price['High'],
+              'Low':stock_price['Low'],
+              'Volume':stock_price['Volume'],
+              'Subjectivity':sen,
+              'Polarity':pol,
+              'Negative':SIA['neg'],
+              'Positive':SIA['pos'],
+              'Neutral':SIA['neu'],
+              'Compound':SIA['compound']
+              }
+        # Create the pandas DataFrame
+        df = pd.DataFrame(data)
+        df.reset_index(drop=True,inplace=True)
+        return df
 
 
 data=news_data()
-news=data.news()
-headline=data.news_attribute('headline')
-merged_data=filter.clean_data(headline)
-sen=filter.getSubjectivity()
-pol=filter.getPolarity()
-SIA=filter.getSIA()
-
-stock_price=yahoo_data.today_yahoo_data()
-
-data={'Open':stock_price['Open'],
-      'High':stock_price['High'],
-      'Low':stock_price['Low'],
-      'Volume':stock_price['Volume'],
-      'Subjectivity':sen,
-      'Polarity':pol,
-      'Negative':SIA['neg'],
-      'Positive':SIA['pos'],
-      'Neutral':SIA['neu'],
-      'Compound':SIA['compound']
-      }
-# Create the pandas DataFrame
-df = pd.DataFrame(data)
-df.reset_index(drop=True,inplace=True)
-print(df)
+dataframe=data.metrics('FB','headline')
+print(dataframe)
+json=data.news_json_api()
+attribute=data.news_attribute('headline')
+print(attribute)
